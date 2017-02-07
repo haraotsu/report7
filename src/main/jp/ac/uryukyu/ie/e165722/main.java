18feb1aa1875f75a3665ca1cad729f616cb446c9
Taiki HARA <e165722@ie.u-ryukyu.ac.jp>
2017-02-07 18:57:45 +0900

    課題レポート7

--- src/main/jp/ac/uryukyu/ie/e165722/main.java
+++ src/main/jp/ac/uryukyu/ie/e165722/main.java
@@ -1,23 +1,20 @@
 package jp.ac.uryukyu.ie.e165722;
 
 import java.lang.Math;
-import java.io.IOException;
 import java.lang.String;
 import java.lang.System;
-import java.io.InputStreamReader;
-import java.io.*;
 import java.util.Scanner;
 
 
 //yはお金（円）の頭文字
 
-public class main{
+public class main {
 
-    public static void main (String args[]){
+    public static void main(String args[]) {
 
         int y0 = 100;
 
-        if(y0 > 0){
+        if (y0 > 0) {
 
             int y2 = 0;
 
@@ -32,11 +29,19 @@ public class main{
 
             System.out.println("掛金は" + str + "です。");
 
-
-
-
-
         }
+
+
+        int n1, n2, n3;
+
+        n1 = (int) (Math.random() * 10);
+        n2 = (int) (Math.random() * 10);
+        n3 = (int) (Math.random() * 10);
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println("でした。");
+
     }
-    
 }
